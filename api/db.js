b10afa19c6e77a489127cd23f098368b1a42643f
Chris154 <c.ohagan@kainos.com>
2018-10-26 13:17:52 +0100

    Added the db query to add a user

--- api/db.js
+++ api/db.js
@@ -13,6 +13,15 @@ db.connect(function(err) {
 })
 
 exports.addUser = (data, callback) => {
+    db.query(
+        'INSERT INTO Employee (department_id, first_name, last_name, address, nin, iban, starting_salary) \
+         VALUES (?, ?, ?, ?, ?, ?, ?)',
+         [data.department_id, data.first_name, data.last_name, data.address, data.nin, data.iban, data.starting_salary],
+         function(err, results, fields){
+            if(err) throw err;
+            callback(results.insertId)
+         }
+    )
 }
 
 exports.getUsers = (callback) => {
